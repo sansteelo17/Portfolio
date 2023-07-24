bb94ea4365f59e78186c968b9b42d1c764b31c59
George <gakpovwovwo@gmail.com>
2023-07-24 14:06:58 +0100

    latest changes

--- src/helpers/projects.js
+++ src/helpers/projects.js
@@ -6,6 +6,14 @@ import {
 
 export const projects = [
   {
+    title: "AI-Summarizer",
+    desc: "An open-source article summarizer that transforms lengthy articles into clear and concise summaries using OpenAI's GPT-4",
+    image: "/images/Screenshot_20230724_125459.png",
+    tech: [faReact],
+    link: "https://gentle-syrniki-aad3d1.netlify.app",
+    id: 0,
+  },
+  {
     title: "E-Commerce",
     desc: "A fully functional E-Commerce webapp deployed on Firebase using React, Redux Toolkit and TailwindCSS",
     image: "/images/Screenshot_20230215_014034.png",
@@ -30,11 +38,51 @@ export const projects = [
     id: 3,
   },
   {
+    title: "AI Keyword Extractor",
+    desc: "Get the most important keywords from any text you put in. Developed using Open AI. Developed using React + TypeScript",
+    image: "/images/Screenshot_20230724_125916.png",
+    tech: [faReact],
+    link: "https://lustrous-sunburst-eb5e3e.netlify.app/",
+    id: 4,
+  },
+  {
+    title: "Steelo Gym",
+    desc: "Fitness web application built using typescript + react",
+    image: "/images/Screenshot_20230724_130244.png",
+    tech: [faReact],
+    link: "https://steelo-gym.web.app/",
+    id: 5,
+  },
+  {
+    title: "Goal Setter App",
+    desc: "Full stack MERN goal setter app using Redux Toolkit where user can sign up and add goals. JWT for authentication",
+    image: "/images/Screenshot_20230724_134709.png",
+    tech: [faReact, faNode],
+    link: "https://github.com/sansteelo17/goal-setter-app",
+    id: 6,
+  },
+  {
+    title: "QR Code Generator",
+    desc: "A QR Code generator using vanilla Javascript. Insert your website URL and get a custom QR Code users can scan to access it",
+    image: "/images/Screenshot_20230724_135858.png",
+    tech: [faSquareJs],
+    link: "https://github.com/sansteelo17/qr-code-generator",
+    id: 7,
+  },
+  {
     title: "Posts",
     desc: "A web application using JavaScript (NODE & EJS), MongoDB and Express and Passport for user authentication where users can add or delete a post, make posts public or private, and review other posts.",
     image: "/images/Screenshot_20230215_014759.png",
     tech: [faSquareJs, faNode],
     link: "https://github.com/sansteelo17/Posts",
-    id: 4,
+    id: 8,
+  },
+  {
+    title: "Steelobnb",
+    desc: "Add reservations, book trips, and list your properties on this fully functional Airbnb Clone Application. Sign up or Login using your Google or Github account. Developed using Nextjs + TypeScript, Primsa + MongoDb Atlas, World Countries for maps and TailwindCSS for styling",
+    image: "/images/Screenshot_20230724_124420.png",
+    tech: [faReact],
+    link: "https://steelobnb.vercel.app",
+    id: 9,
   },
 ];
